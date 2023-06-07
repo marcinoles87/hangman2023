c4a0c5c932b9346cd2e6942c23f33728ee38a00e
marcinoles87 <marcinoles87@gmail.com>
2023-06-07 16:08:40 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react-dom'
 import './App.css';
 import Header from './components/Header.js';
+import Figure from './components/Figure.js';
 
 function App() {
   return (
     <div className="App">
        <Header></Header>
+       <div className='game-container'>
+          <Figure></Figure>
+       </div>
+
     </div>
   );
 }
