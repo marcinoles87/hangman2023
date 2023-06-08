59974a857f4cbc2829455d97feb89ebed83f3163
marcinoles87 <marcinoles87@gmail.com>
2023-06-08 10:12:14 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import Figure from './components/Figure.js';
 import WrongLetters from './components/WrongLetters.js';
 import Word from './components/Word';
 
+
+const words = ['application' , 'programing' , 'wizard' , 'polska'] ;
+console.log(words)
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+console.log(selectedWord)
+
 function App() {
+
+
   return (
     <div className="App">
        <Header></Header>
