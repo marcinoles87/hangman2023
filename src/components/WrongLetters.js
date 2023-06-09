afd56aaa653a08299a0ce9294bf98181c76eb1e7
marcinoles87 <marcinoles87@gmail.com>
2023-06-09 12:23:51 +0200

    add letter , add wrong letter

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function WrongLetters({wrongLetter}) {
+function WrongLetters({wrongLetter , }) {
   return (
     <div className='wrongletters-container'>
         <div className='wrong-letters'>
-          {wrongLetter}
+          {wrongLetter.length > 0 && <p>Wrong</p>}
+          {wrongLetter
+          .map( (letter , index) => <span key={index}>{letter}</span>)
+          .reduce( (prev , current) => prev === null ? [current] : [prev , ',' , current] , null)}
         </div>
         </div>
   )
