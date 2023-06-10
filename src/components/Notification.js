49f3247481e86171ed001fefc6c74d110e298f1c
marcinoles87 <marcinoles87@gmail.com>
2023-06-10 10:47:16 +0200

    add new components

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Notification({showNotification}) {
   return (
-    <div className={showNotification ? 'show' : ''} id='notification-container'>Notification :
+    <div className={`notification-container ${showNotification ? 'show' : ''} `}>Notification :
         <p>you have already use this letter</p>
     </div>
   )
